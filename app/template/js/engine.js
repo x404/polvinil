49f04ef1694d99dc8f685926035e3e39d7798681
Aleksey <kronos2003@gmail.com>
2019-01-11 16:28:59 +0200

    модаьная форма "консультация по телефону"

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -77,4 +77,129 @@ $(document).ready(function(){
 			};
 		init();
 	});	
+
+
+
+	// VALIDATE
+	// validate
+	$.validator.addMethod("validphone", function(value){
+		if (Inputmask.isValid(value, { mask: '+7 ( 999 ) 999 - 99 - 99'})) return true
+		else return false;
+	},"");
+
+
+	var thankcallback = '<div class="thank text-center"><p>В ближайщее время с вами свяжутся наши менеджеры для уточнения всех деталей</p></div>';
+	var thankfaq = '<div class="thank text-center"><p>Ваш вопрос отправлен</p></div>';
+	var thankreview = '<div class="thank text-center"><p>Ваш отзыв отправлен</p></div>';
+	var errorTxt = 'Форма не отправлена. Попробуйте позже.';
+	// validation forms
+	$('#callback-form').validate({
+        rules : {
+            tel:{validphone:true}           
+        },
+		submitHandler: function(form){
+			var strSubmit=$(form).serialize();
+			$(form).find('fieldset').hide();
+			$(form).append('<div class="sending">Идет отправка ...</div>');
+
+			$.ajax({
+				type: "POST",
+				url: $(form).attr('action'),
+				data: strSubmit,
+				success: function(){
+					document.querySelector('.sending').remove();
+					$(form).append(thankcallback);
+					startClock('callback-form');
+				},
+				error: function(){
+					alert(errorTxt);
+					$(form).find('fieldset').show();
+					$('.sending').remove();
+				}
+			})
+			.fail(function(error){
+				alert(errorTxt);
+			});
+		}
+	});
 });
+
+
+
+$(function(){
+	$('.policy input').click(function(){
+		var $this = $(this),
+			$submit = $this.closest('.form-policy');
+
+		if ($this.is(':checked')){
+			$submit.find('.input, .form-control, .submit, textarea, input[type=radio]').removeAttr('disabled');
+		} else {
+			$submit.addClass('disabled');
+			$submit.find('.input, .form-control, .submit, textarea, input[type=radio]').attr('disabled', true);
+		}
+	})
+});
+
+$(function(){
+	// mask
+	$('input.tel').inputmask({
+	        mask: '+7 ( 999 ) 999 - 99 - 99',
+	        showMaskOnHover : false
+	});
+	// #mask
+});
+
+
+
+
+
+var timer,
+	sec = 3;
+
+
+function showTime(sendform){
+	sec = sec-1;
+	if (sec <=0) {
+		stopClock();
+
+		switch (sendform){
+			// case 'qorder-form':
+			// 	$('.qorder__box .thank').fadeOut('normal',function(){
+			// 		$('.qorder__box .thank').remove();
+			// 		$('.qorder__box .form-control, .qorder__box textarea').val('');
+			// 	});
+			// 	break;
+			case 'callback-form':
+				$('#callback .thank').fadeOut('normal',function(){
+					$('#callback .thank').remove();
+					$('#callback .form-control, #callback textarea').val('');
+					$('#callback fieldset').show();
+				});
+				break;
+			// case 'cart-form':
+			// 	$('.cart .thank').fadeOut('normal',function(){
+			// 		$('.cart .thank').remove();
+			// 		// $('.cart .form-control, .cart textarea').val('');
+			// 		// $('.cart__form fieldset').show();
+			// 	});
+			// 	break;	
+			default:
+				modal = $("#" + sendform).closest('.modal');
+				modal.modal('hide');
+				modal.find('.thank').remove();
+				modal.find('.form-control, textarea').val('');
+				break;
+		}
+	}
+}
+
+function stopClock(){
+	window.clearInterval(timer);
+	timer = null;
+	sec = 3;
+}
+
+function startClock(sendform){
+	if (!timer)
+		timer = window.setInterval("showTime('" + sendform + "')",1000);
+}
