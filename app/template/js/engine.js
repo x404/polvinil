0e7894b8366474525a7b7ae70faeda535dbfd2ac
Aleksey <kronos2003@gmail.com>
2019-01-17 16:52:41 +0200

    Добавлены услуги/услуга, адаптив.

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -316,6 +316,89 @@ $(document).ready(function(){
 	})
 
 
+	// inner page
+	$('#gallery-carousel').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev slick-arrow slick-arrow-gray" aria-label="Назад"><svg width="17" height="31"><line x1="17" y1="0" x2="0" y2="17" /><line x1="0" y1="15" x2="17" y2="31" /></svg></button>',
+		nextArrow: '<button type="button" class="slick-next slick-arrow slick-arrow-gray" aria-label="Вперед"><svg width="17" height="31"><line x1="0" y1="0" x2="17" y2="17" /><line x1="17" y1="15" x2="0" y2="31" /></svg></button>',
+		responsive: [
+		    {
+		      breakpoint: 1399,
+		      settings: {
+		        slidesToShow: 4,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 991,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 768,
+		      settings: {
+				arrows: false,
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+				centerMode: true,
+				centerPadding: '90px'
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+				centerMode: true,
+				centerPadding: '100px',
+				arrows: false
+		      }
+		    },
+		    {
+		      breakpoint: 450,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+				centerMode: true,
+				centerPadding: '60px',
+				arrows: false
+		      }
+		    },
+		    {
+		      breakpoint: 390,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+				centerMode: true,
+				centerPadding: '60px',
+				arrows: false
+		      }
+		    },
+		    {
+		      breakpoint: 350,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+				centerMode: true,
+				centerPadding: '40px',
+				arrows: false
+		      }
+		    },
+		    {
+		      breakpoint: 0,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+				centerMode: true,
+				centerPadding: '30px',
+				arrows: false
+		      }
+		    }
+		  ]
+	});	
 
 	// mobile-menu
 	$('#navbar').each(function(){
